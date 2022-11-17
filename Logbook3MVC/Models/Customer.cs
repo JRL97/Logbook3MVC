af998232b0f45c5e1525386bef78dc4b65262028
JRL97 <91621546+JRL97@users.noreply.github.com>
2022-11-17 10:51:06 +0000

    Added Address to Customer

--- Logbook3MVC/Models/Customer.cs
+++ Logbook3MVC/Models/Customer.cs
@@ -1,10 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Logbook3MVC.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+
+        
+        public int AddressID { get; set; }
 
         [Required, StringLength(20)]
         public string Name { get; set; } = String.Empty;
@@ -14,5 +18,9 @@ namespace Logbook3MVC.Models
 
         [StringLength(20), DataType(DataType.PhoneNumber)]
         public string? PhoneNumber { get; set; } = null;
+
+        // Navigation Properties
+
+        public Address? Address { get; set; } 
     }
 }
