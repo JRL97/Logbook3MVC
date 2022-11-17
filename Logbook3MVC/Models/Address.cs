6ad39507cc3d7806fad6cec0346bddf42a8871b5
JRL97 <91621546+JRL97@users.noreply.github.com>
2022-11-17 10:39:50 +0000

    Added and annotated address

---
+++ Logbook3MVC/Models/Address.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Logbook3MVC.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(20)]
+        public string HouseNumber { get; set; } = String.Empty;
+
+        [Required, StringLength(10)]
+        public string PostCode { get; set; } = String.Empty;
+    }
+}
