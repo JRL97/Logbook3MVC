7522218c7e4ac1a370e396745cc955e2c8aa6cd8
JRL97 <91621546+JRL97@users.noreply.github.com>
2022-11-17 10:21:51 +0000

    Add project files.

---
+++ Logbook3MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Logbook3MVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Logbook3MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Logbook3MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
