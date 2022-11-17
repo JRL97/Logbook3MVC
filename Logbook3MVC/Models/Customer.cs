4ff00873b6541b15a69a33413b8824959d45fbf9
JRL97 <91621546+JRL97@users.noreply.github.com>
2022-11-17 10:35:00 +0000

    Annotated Customer

---
+++ Logbook3MVC/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Logbook3MVC.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(20)]
+        public string Name { get; set; } = String.Empty;
+
+        [Required, StringLength(20), DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = String.Empty;
+
+        [StringLength(20), DataType(DataType.PhoneNumber)]
+        public string? PhoneNumber { get; set; } = null;
+    }
+}
